86018293be12e90656d36be7463df21dd00ed4ff
辣橙 <2625835752@qq.com>
2022-03-24 15:24:38 +0800

    v0.1.0

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import time
+from threading import Thread
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PySide6.QtWidgets import QApplication, QMainWindow
+# PySide6-uic demo.ui -o ui_demo.py
+# from ui_demo import Ui_Demo
+from task import add
+from ui_demo import Ui_MainWindow
+
+from Signal import my_signal
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()  # UI类的实例化()
+        self.ui.setupUi(self)
+        self.band()
+
+    def band(self):
+        # self.ui.___ACTION___.triggered.connect(___FUNCTION___)
+        # self.ui.___BUTTON___.clicked.connect(___FUNCTION___)
+        # self.ui.___COMBO_BOX___.currentIndexChanged.connect(___FUNCTION___)
+        # self.ui.___SPIN_BOX___.valueChanged.connect(___FUNCTION___)
+        # 自定义信号.属性名.connect(___FUNCTION___)
+
+        self.ui.pushButton.clicked.connect(self.handle_click)
+
+        my_signal.setProgressBar.connect(self.set_progress_bar)
+
+        my_signal.setResult.connect(self.set_result)
+
+    def set_result(self, result: str):
+        self.ui.result.setText(result)
+
+    def set_progress_bar(self, progress: int):
+        self.ui.progressBar.setValue(progress)
+
+    def handle_click(self):
+        def innerFunc():
+            a = self.ui.inputA.value()
+            b = self.ui.inputB.value()
+
+            time_cost = self.ui.timeCost.value()
+
+            for index, _ in enumerate(range(time_cost)):
+                progress = index * 100 // time_cost
+                my_signal.setProgressBar.emit(progress)
+                time.sleep(1)
+            my_signal.setProgressBar.emit(100)
+
+            result = str(add(a, b))
+            my_signal.setResult.emit(result)
+
+        task = Thread(target=innerFunc)
+        task.start()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication([])  # 启动一个应用
+    window = MainWindow()  # 实例化主窗口
+    window.show()  # 展示主窗口
+    app.exec()  # 避免程序执行到这一行后直接退出
